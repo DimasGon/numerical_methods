324722c83d2ebc1f833d026565f876a7ff3ae116
DimasGon <dima.gonchar.29.08.13@gmail.com>
2018-12-18 16:47:47 +0300

    Предварительная реализация (есть ошибки)

--- lab1/forms.py
+++ lab1/forms.py
@@ -8,15 +8,14 @@ class InputDataForm(forms.Form):
         ('Кранка-Николсона', 'Кранка-Николсона')
     )
     CHOICES_APPROXIMATION = (
-        ('type-1', 'Двухточечная 1-ого порядка'),
-        ('type-2', 'Двухточечная 2-ого порядка'),
-        ('type-3', 'Трехточечная 2-ого порядка'),
+        ('Двухточечная 1-ого порядка', 'Двухточечная 1-ого порядка'),
+        ('Двухточечная 2-ого порядка', 'Двухточечная 2-ого порядка'),
+        ('Трехточечная 2-ого порядка', 'Трехточечная 2-ого порядка'),
     )
 
     method = forms.ChoiceField(label='Выберите метод решения', choices=CHOICES_METHOD, widget=forms.RadioSelect, required=True)
     approximation = forms.ChoiceField(label='Выберите аппроксимацию', choices=CHOICES_APPROXIMATION, widget=forms.RadioSelect, required=True)
     a = forms.FloatField(label='Константа a', required=True)
-    # x_end = forms.FloatField(label='Окончание отчета по иксу', required=True)
     t_end = forms.FloatField(label='Окончание по времени', required=True)
     num_split = forms.FloatField(label='Количество разбиений икса', required=True)
     sigma = forms.FloatField(label='Число Куранта', required=True)

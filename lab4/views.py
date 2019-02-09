7dd9d1515ec61cf5caa63376520f0da494e97da9
DimasGon <dima.gonchar.29.08.13@gmail.com>
2019-02-09 10:30:42 +0300

    Чистка мусора

--- lab4/views.py
+++ lab4/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 from django.views.generic import View
 import os
 from .forms import InputDataForm
-from .laba import solve
 
 class Lab4View(View):
 
@@ -15,29 +14,4 @@ class Lab4View(View):
         form = InputDataForm()
         return render(request, self.template_name, {
             'form': form,
-        })
-
-    def post(self, request):
-
-        method = request.POST.get('method')
-        a = float(request.POST.get('a'))
-        t_end = float(request.POST.get('t_end'))
-        num_split_t = int(request.POST.get('num_split_t'))
-        num_split_x = int(request.POST.get('num_split_x'))
-        num_split_y = int(request.POST.get('num_split_y'))
-
-        solve(method, a, t_end, num_split_t, num_split_x, num_split_y)
-        root_dir = os.path.abspath(os.curdir)
-        os.remove(root_dir + '/common_static/img/graph_0.png')
-        os.remove(root_dir + '/common_static/img/graph_mid.png')
-        os.remove(root_dir + '/common_static/img/error_0.png')
-        os.remove(root_dir + '/common_static/img/error_mid.png')
-        os.rename(root_dir + '/graph_0.png', root_dir + '/common_static/img/graph_0.png')
-        os.rename(root_dir + '/graph_mid.png', root_dir + '/common_static/img/graph_mid.png')
-        os.rename(root_dir + '/error_0.png', root_dir + '/common_static/img/error_0.png')
-        os.rename(root_dir + '/error_mid.png', root_dir + '/common_static/img/error_mid.png')
-        form = InputDataForm(request.POST)
-
-        return render(request, self.template_name, {
-            'form': form, 'show_graph': True,
         })
